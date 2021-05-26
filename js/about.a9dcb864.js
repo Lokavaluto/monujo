07a459ba0d7d89b71d4d3429a3265cefaefc1baf
Gaspard "Sarp" Lonchampt <71872843+gaspard-lonchampt@users.noreply.github.com>
2021-05-26 14:27:46 +0200

    'deploy'

---
+++ js/about.a9dcb864.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,e){"use strict";e.r(t);var a=e("7a23"),c={class:"about"},o=Object(a["i"])("h1",null,"This is an about page",-1);function s(n,t){return Object(a["r"])(),Object(a["e"])("div",c,[o])}const u={};u.render=s;t["default"]=u}}]);
+//# sourceMappingURL=about.a9dcb864.js.map
