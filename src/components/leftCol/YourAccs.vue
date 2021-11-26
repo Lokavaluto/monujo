4d23c50f0a8a6c0d34c4287e7dfe80658c9d9415
Gwen Coffy <gwen@otavu.com>
2021-11-26 20:02:21 +0100

    Make Accounts list provide a message to the user when no account is defined for him

--- src/components/leftCol/YourAccs.vue
+++ src/components/leftCol/YourAccs.vue
@@ -4,9 +4,14 @@
       class="card custom-card custom-card-padding"
     >
       <h2 class="custom-card-title">vos comptes</h2>
-      <Acc v-for="account in moneyAccounts" :bal="account.bal" :curr="account.curr">
-        <template v-slot:name>{{ account.name.split(':')[0] }}</template>
-      </Acc>
+      <template v-if="moneyAccounts.length > 0">
+        <Acc v-for="account in moneyAccounts" :bal="account.bal" :curr="account.curr">
+          <template v-slot:name>{{ account.name.split(':')[0] }}</template>
+        </Acc>
+      </template>
+      <template v-else>
+        <p class="notification is-default">Vous n'avez pas encore de compte</p>
+      </template>
     </div>
     <!-- fin card vos comptes -->
 </template>
