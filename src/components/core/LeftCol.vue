000b5f2395c732c07270a3b7d107276d289b3966
Valentin Lab <valentin.lab@kalysto.org>
2021-11-30 22:13:47 +0100

    chg: dev: remove global balance display
    The global balance was redundant with the display of accounts.
    
    Signed-off-by: Valentin Lab <valentin.lab@kalysto.org>

--- src/components/core/LeftCol.vue
+++ src/components/core/LeftCol.vue
@@ -1,7 +1,6 @@
 <template>
   <!-- colonne de gauche -->
         <div class="column">
-          <GlobalBal class="mb-4"/>
           <YourAccs/>
           <!-- <PayCards/> -->
         </div>
@@ -10,14 +9,12 @@
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import GlobalBal from "../leftCol/GlobalBal.vue";
 import YourAccs from "../leftCol/YourAccs.vue";
 //import PayCards from "../leftCol/PayCards.vue"
 
 @Options({
   name: "LeftCol",
   components: {
-    GlobalBal,
     YourAccs,
     // PayCards
   }
