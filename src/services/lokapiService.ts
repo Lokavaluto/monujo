8775bfed0215b36f8012ecf4d68cbf3910baa265
Valentin Lab <valentin.lab@kalysto.org>
2021-12-03 22:49:38 +0100

    new: dev: [store] pass-on LokAPI's exceptions in service
    We'll need to be able to catch specific exception from LokAPI, as
    such, the service should allow us to refer to them.
    
    Signed-off-by: Valentin Lab <valentin.lab@kalysto.org>

--- src/services/lokapiService.ts
+++ src/services/lokapiService.ts
@@ -1,4 +1,4 @@
-import { LokAPIBrowserAbstract } from '@lokavaluto/lokapi-browser'
+import { LokAPIBrowserAbstract, e } from '@lokavaluto/lokapi-browser'
 import comchain from '@lokavaluto/lokapi-backend-comchain'
 import cyclos from '@lokavaluto/lokapi-backend-cyclos'
 import Swal from "sweetalert2"
@@ -168,3 +168,4 @@ export function replaceOrInsertElt<T>(array: Array<T>, elt: T,
 }
 
 
+export const LokAPIExc = e
