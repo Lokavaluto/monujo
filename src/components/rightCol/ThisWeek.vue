0dcc1d0ac4d0f8b822ddae9934fe27314d73719a
Gwen Coffy <gwen@otavu.com>
2021-12-16 13:51:29 +0100

    fix: [component] indentation and useless comments

--- src/components/rightCol/ThisWeek.vue
+++ src/components/rightCol/ThisWeek.vue
@@ -1,15 +1,13 @@
 <template>
-     <!-- card cette semaine  -->
-            <h2 class="custom-card-sub-title">Cette semaine</h2>
-            <TransactionSubCard v-for="transaction in getTrs"
-             :key="transaction" :amount="transaction.amount" 
-             :symbol="transaction.currency"
-             :desc="transaction.description"
-             :date="dateFormated(transaction.date)"
-             :unformatedDate="transaction.date"
-             :name="transaction.relatedUser ? transaction.relatedUser.display : transaction.related.type.name "
-             picto="QR"/>
-            <!-- fin card cette semaine -->
+    <h2 class="custom-card-sub-title">Cette semaine</h2>
+    <TransactionSubCard v-for="transaction in getTrs"
+        :key="transaction" :amount="transaction.amount" 
+        :symbol="transaction.currency"
+        :desc="transaction.description"
+        :date="dateFormated(transaction.date)"
+        :unformatedDate="transaction.date"
+        :name="transaction.relatedUser ? transaction.relatedUser.display : transaction.related.type.name "
+        picto="QR"/>
 </template>
 
 <script lang="ts">
@@ -38,26 +36,10 @@ import TransactionSubCard from "./TransactionSubCard.vue"
         dateFormated(badDate:string) :string {
             var date = new Date(badDate);
             var options = {weekday: "long", day: "numeric", month: "numeric"};
-            // eslint-disable-next-line
-            //@ts-ignore-next-line
             const DateFr = new Intl.DateTimeFormat('fr-FR', options).format(date)
             return DateFr
         }
     },
-    // mounted: async function () {
-    //     let transactions = this.$store.getters.getTransactions()
-    //     var maxTransactions = 5
-    //     for (let el of transactions) {
-    //         if (el.relatedUser) {
-    //             this.transactions.push(el)
-    //             if (maxTransactions === 1) {
-    //                 break;
-    //             }
-    //             maxTransactions -= 1
-    //         }
-    //     }
-    //     console.log(this.transactions
-    // }
 })
 export default class ThisWeek extends Vue {}
 </script>
