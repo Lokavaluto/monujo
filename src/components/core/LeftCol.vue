2062e4edb1f52c44daaded7ccbf0f5efcf8c95fd
Gwen Coffy <gwen@otavu.com>
2021-12-16 14:19:52 +0100

    fix: [component] remove useless comments and fix indentation

--- src/components/core/LeftCol.vue
+++ src/components/core/LeftCol.vue
@@ -1,22 +1,17 @@
 <template>
-  <!-- colonne de gauche -->
-        <div class="column">
-          <YourAccs/>
-          <!-- <PayCards/> -->
-        </div>
-        <!-- fin colonne de gauche -->
+    <div class="column">
+        <YourAccs/>
+    </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import YourAccs from "../leftCol/YourAccs.vue";
-//import PayCards from "../leftCol/PayCards.vue"
 
 @Options({
   name: "LeftCol",
   components: {
-    YourAccs,
-    // PayCards
+    YourAccs
   }
 })
 export default class LeftCol extends Vue {}
