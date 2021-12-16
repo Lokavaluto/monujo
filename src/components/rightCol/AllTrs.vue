3491effab35e58e6087b85e35badf86f5f9c19f3
Gwen Coffy <gwen@otavu.com>
2021-12-16 14:20:56 +0100

    chg: [component] show a loading indicator while loading transactions

--- src/components/rightCol/AllTrs.vue
+++ src/components/rightCol/AllTrs.vue
@@ -1,29 +1,47 @@
 <template>
-<h2 class="custom-card-sub-title has-text-centered">Toutes les opérations</h2>
-<div>
-    <TransactionSubCard v-for="transaction in getTrs"
-        :key="transaction" :amount="transaction.amount" 
-        :symbol="transaction.currency"
-        :desc="transaction.description"
-        :date="dateFormated(transaction.date)"
-        :unformatedDate="transaction.date"
-        :name="transaction.relatedUser ? transaction.relatedUser.display : transaction.related.type.name "
-        picto="QR"/>
-</div>
+    <div
+      class="accounts card custom-card custom-card-padding"
+    >
+        <h2 class="custom-card-title">Operations</h2>
+        <div class="notification p-6" v-if="isLoadingTransactions">
+            <loading v-model:active="isLoadingTransactions"
+                :can-cancel="false"
+                :is-full-page="false"
+            />
+        </div>
+
+        <TransactionSubCard v-for="transaction in thisweekTransactions"
+            :key="transaction" :amount="transaction.amount" 
+            :symbol="transaction.currency"
+            :desc="transaction.description"
+            :date="dateFormated(transaction.date)"
+            :unformatedDate="transaction.date"
+            :name="transaction.relatedUser ? transaction.relatedUser.display : transaction.related.type.name "
+            picto="QR"/>
+    </div>
 </template>
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import TransactionSubCard from "./TransactionSubCard.vue";
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 @Options({
 
     name:"AllTrs",
     components: {
-        TransactionSubCard
+        TransactionSubCard,
+        Loading
     },
     computed: {
-        getTrs(): number {
+        thisweekTransactions(): number {
+            return this.$store.state.lokapi.thisWeektransactions
+        },
+        upcomingTransactions(): number {
             return this.$store.state.lokapi.transactions
+        },
+        isLoadingTransactions():boolean {
+            return this.$store.state.lokapi.transactionsStatus === 'loading'
         }
     },
     methods : { 
