9af348fe56ad6ea5d7e3ed52946e64ef36013cd9
Gwen Coffy <gwen@otavu.com>
2021-12-16 14:19:14 +0100

    chg: [component] remove the modal listing the transactions as we are going to make it an infinite scroll

--- src/components/core/RightCol.vue
+++ src/components/core/RightCol.vue
@@ -1,41 +1,7 @@
 <template>
-  <!-- colonne de droite -->
-  <div class="column">
-    <div
-      class="card custom-card custom-card-padding"
-    >
-      <div class="is-flex-direction-column is-align-items-center is-justify-content-space-between">
-        <OperationsSelector />
-        <ThisWeek />
-      </div>
-      <MyModal :first="true" v-if="showModal" @close="showModal = false">
-        <template v-slot:header>
-         <div></div>
-        </template>
-        <template v-slot:body>
-          <div
-            class="card custom-card is-flex-direction-column is-align-items-center is-justify-content-space-between custom-card-padding mb-4"
-          >
-           <!-- <h2 class="custom-card-sub-title-fixed-top">La semaine dernière</h2> -->
-            <!-- <LastWeek /> -->
-            <AllTrs/>
-          </div>
-        </template>
-        <template v-slot:footer>
-          <div></div>
-        </template>
-      </MyModal>
-      <div class="has-text-centered mt-5">
-        <button
-          @click="showModal = true"
-          class="button custom-button custom-inverted"
-        >
-          Voir tout
-        </button>
-      </div>
+    <div class="column">
+        <AllTrs/>
     </div>
-  </div>
-  <!-- fin colonne de droite -->
 </template>
 
 <script lang="ts">
@@ -49,11 +15,7 @@ import MyModal from "../modal/MyModal.vue";
 @Options({
   name: "RightCol",
   components: {
-    OperationsSelector,
-    ThisWeek,
-    //LastWeek,
-    AllTrs,
-    MyModal,
+    AllTrs
   },
 
   data() {
