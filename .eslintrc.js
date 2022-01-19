94ccb05fdd4f11fd41ff0262791db95f40ea6368
Valentin Lab <valentin.lab@kalysto.org>
2022-01-19 17:52:02 +0100

    fix: pkg: enforce indentation
    Signed-off-by: Valentin Lab <valentin.lab@kalysto.org>

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,17 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'indent': 'off',
+    'vue/script-indent': ['warn', 2, {
+      'baseIndent': 1
+    }],
+    'vue/html-indent': ['error', 2, {
+      'attribute': 1,
+      'baseIndent': 1,
+      'closeBracket': 0,
+      'alignAttributesVertically': true,
+      'ignores': []
+    }]
   }
 }
