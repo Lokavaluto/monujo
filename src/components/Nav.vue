8e0367ccd15ddc48c76870454b8e41f315e3ec72
Gwen Coffy <gwen@otavu.com>
2022-02-01 10:15:07 +0100

    new: add carto entry for web and mobile app
    and fixes indentation
    
    Signed-off-by: Gwen Coffy <gwen@otavu.com>

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,10 +1,14 @@
 <template>
-  <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+  <nav
+    class="navbar is-fixed-top"
+    role="navigation"
+    aria-label="main navigation"
+  >
     <div class="navbar-brand">
       <router-link :to="{ name: 'dashboard' }" class="navbar-item">
         <img
           class="is-rounded"
-          style="max-height:none !important"
+          style="max-height: none !important"
           src="https://lokavaluto.fr/web/image/res.company/1/logo?unique=2eaba5c"
           width="80"
           height="80"
@@ -31,14 +35,18 @@
       class="navbar-menu"
       :class="{ 'is-active': showNav }"
     >
-
       <div class="navbar-end">
-
-        <div v-if="getLog && hasUserAccountValidationRights" class="navbar-item has-dropdown is-hoverable">
+        <router-link to="/carto" class="navbar-item"> Carto </router-link>
+        <router-link to="/" class="navbar-item" v-if="!getLog">
+          Se connecter
+        </router-link>
+        <div
+          v-if="getLog && hasUserAccountValidationRights"
+          class="navbar-item has-dropdown is-hoverable"
+        >
           <a class="navbar-link">
             Admin
           </a>
-
           <div class="navbar-dropdown is-right">
             <router-link to="/admin/pending-accounts" class="navbar-item">
               Comptes
@@ -48,65 +56,66 @@
 
         <div class="navbar-item has-dropdown is-hoverable" v-if="getLog">
           <a class="navbar-link">
-            {{ userProfile?.name ? userProfile.name : 'Utilisateur' }}
+            {{ userProfile?.name ? userProfile.name : "Utilisateur" }}
           </a>
 
           <div class="navbar-dropdown is-right">
             <a :href="profilePageUrl" target="_blank" class="navbar-item">
               Profil
             </a>
-            <router-link v-if="hasUnconfiguredBackend" to="/create-account" class="navbar-item">
+            <router-link
+              v-if="hasUnconfiguredBackend"
+              to="/create-account"
+              class="navbar-item"
+            >
               Créer mon portefeuille
             </router-link>
 
-            <hr class="navbar-divider">
+            <hr class="navbar-divider" />
 
-            <a @click="logout" class="navbar-item">
-              Déconnexion
-            </a>
+            <a @click="logout" class="navbar-item"> Déconnexion </a>
           </div>
         </div>
-
       </div>
     </div>
   </nav>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { Options, Vue } from "vue-class-component";
 
 @Options({
   name: "Nav",
   data() {
     return {
-      showNav: false
+      showNav: false,
     };
   },
-  methods : {
+  methods: {
     logout() {
-      this.$store.dispatch("askLogOut")
-      this.$router.push({ name: 'Login' })
-    }
+      this.$store.dispatch("askLogOut");
+      this.$router.push({ name: "Carto" });
+    },
   },
   computed: {
-      getLog(): string {
-         return this.$store.state.lokapi.isLog
-      },
-      hasUnconfiguredBackend(): boolean {
-        // Display of the account creation button should be displayed only
-        // if there's an un-configured backend
-        return this.$store.getters.hasUnconfiguredBackend()
-      },
-      hasUserAccountValidationRights(): boolean {
-        return this.$store.state.lokapi.hasUserAccountValidationRights
-      },
-      userProfile(): string {
-        return this.$store.state.lokapi.userProfile
-      },
-      profilePageUrl(): string {
-        return this.$store.getters.getOdooUrl() + '/fr_FR/my/home'
-      }
+    getLog(): string {
+      return this.$store.state.lokapi.isLog;
     },
+    hasUnconfiguredBackend(): boolean {
+      // Display of the account creation button should be displayed only
+      // if there's an un-configured backend
+      return this.$store.getters.hasUnconfiguredBackend();
+    },
+    hasUserAccountValidationRights(): boolean {
+      return this.$store.state.lokapi.hasUserAccountValidationRights;
+    },
+    userProfile(): string {
+      return this.$store.state.lokapi.userProfile;
+    },
+    profilePageUrl(): string {
+      return this.$store.getters.getOdooUrl() + "/fr_FR/my/home";
+    },
+  },
 })
 export default class Nav extends Vue {}
 </script>
