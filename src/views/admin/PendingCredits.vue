3ac82935d1e62f4b20ca8c9c32a5178ca9b180c3
gwen <gwen@otavu.com>
2022-02-16 18:34:06 +0100

    fix: fix incorrect margin around box in the pending credits list view

--- src/views/admin/PendingCredits.vue
+++ src/views/admin/PendingCredits.vue
@@ -1,48 +1,50 @@
 <template>
-  <div class="container mt-5">
-    <div class="columns is-tablet">
-      <div class="column">
-        <div
-          class="accounts card custom-card custom-card-padding"
-        >
-          <h2 class="custom-card-title">Opérations de crédit en attente de validation</h2>
-          <table class="table is-striped is-fullwidth">
-            <thead>
-              <tr>
-                <th>Utilisateur</th>
-                <th>Montant</th>
-                <th class="has-text-right">Valider</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="request in pendingCreditRequests">
-                <td>
-                  {{request.relatedUser}} {{ request.markBackend ? `(on ${request.backendId})` : ""}}
-                </td>
-                <td>
-                  {{
-                    parseFloat(request.amount).toLocaleString(
-                      "fr", {
-                        minimumFractionDigits: 2,
-                        maximumFractionDigits: 2
-                      })
-                  }} {{request.currency}}
-                </td>
-                <td class="has-text-right">
-                  <a
-                    class="button is-primary custom-button custom-inverted is-small is-pulled-right"
-                    v-on:click="validateCreditRequest(request)"
-                  >
-                    valider
-                  </a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+  <main class="main">
+    <div class="container mt-5">
+      <div class="columns is-tablet">
+        <div class="column">
+          <div
+            class="accounts card custom-card custom-card-padding"
+          >
+            <h2 class="custom-card-title">Opérations de crédit en attente de validation</h2>
+            <table class="table is-striped is-fullwidth">
+              <thead>
+                <tr>
+                  <th>Utilisateur</th>
+                  <th>Montant</th>
+                  <th class="has-text-right">Valider</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="request in pendingCreditRequests">
+                  <td>
+                    {{request.relatedUser}} {{ request.markBackend ? `(on ${request.backendId})` : ""}}
+                  </td>
+                  <td>
+                    {{
+                      parseFloat(request.amount).toLocaleString(
+                        "fr", {
+                          minimumFractionDigits: 2,
+                          maximumFractionDigits: 2
+                        })
+                    }} {{request.currency}}
+                  </td>
+                  <td class="has-text-right">
+                    <a
+                      class="button is-primary custom-button custom-inverted is-small is-pulled-right"
+                      v-on:click="validateCreditRequest(request)"
+                    >
+                      valider
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script lang="ts">
