c15a6d6b16a8b29dfb913caf6be162b53e490994
Gwen Coffy <gwen@otavu.com>
2022-02-21 12:34:51 +0100

    chg: dev: remove comments and unused code and useless dependency
    It will greatly simplify infinite scroll implementation

--- src/components/core/RightCol.vue
+++ src/components/core/RightCol.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- colonne de droite -->
   <div class="column">
     <div
       class="card custom-card custom-card-padding"
@@ -8,23 +7,21 @@
         <OperationsSelector />
         <ThisWeek />
       </div>
-      <MyModal :first="true" v-if="showModal" @close="showModal = false">
-        <template v-slot:header>
-          <div></div>
-        </template>
-        <template v-slot:body>
-          <div
-            class="card custom-card is-flex-direction-column is-align-items-center is-justify-content-space-between custom-card-padding mb-4"
-          >
-            <!-- <h2 class="custom-card-sub-title-fixed-top">La semaine dernière</h2> -->
-            <!-- <LastWeek /> -->
-            <AllTrs/>
+      <div v-show="showModal" class="modal-mask">
+        <div class="overlay" @click="showModal = false"></div>
+        <div class="modal-wrapper">
+          <div class="modal-container">
+            <div class="modal-body">
+              <div
+                class="card custom-card is-flex-direction-column is-align-items-center is-justify-content-space-between custom-card-padding mb-4"
+              >
+                <AllTrs />
+              </div>
+            </div>
           </div>
-        </template>
-        <template v-slot:footer>
-          <div></div>
-        </template>
-      </MyModal>
+        </div>
+      </div>
+
       <div class="has-text-centered mt-5">
         <button
           @click="showModal = true"
@@ -35,36 +32,26 @@
       </div>
     </div>
   </div>
-  <!-- fin colonne de droite -->
 </template>
 
 <script lang="ts">
   import { Options, Vue } from 'vue-class-component';
   import OperationsSelector from "../rightCol/OperationsSelector.vue";
   import ThisWeek from "../rightCol/ThisWeek.vue";
-  //import LastWeek from "../rightCol/LastWeek.vue";
   import AllTrs from "../rightCol/AllTrs.vue";
-  import MyModal from "../modal/MyModal.vue";
 
   @Options({
     name: "RightCol",
     components: {
       OperationsSelector,
       ThisWeek,
-      //LastWeek,
       AllTrs,
-      MyModal,
     },
 
     data() {
       return {
         showModal: false
       };
-    },
-    computed: {
-      opSel(): number {
-        return this.$store.state.OperationsSelector
-      },
     },
   })
   export default class RightCol extends Vue {}
