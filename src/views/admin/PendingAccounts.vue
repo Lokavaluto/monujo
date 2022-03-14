a0682e3fee1f724bfd4c481a5c3d8b9f0333f19f
Seddik Kadi <seddik.kadi@outlook.fr>
2022-03-14 20:44:49 +0100

    add loader and empty list message to PendingAccounts vue

--- src/views/admin/PendingAccounts.vue
+++ src/views/admin/PendingAccounts.vue
@@ -3,19 +3,25 @@
     <div class="container mt-5">
       <div class="columns is-tablet">
         <div class="column">
+           <loading v-model:active="isLoading"
+                 :can-cancel="false"
+                 :is-full-page= "true"/>
           <div
-            class="accounts card custom-card custom-card-padding"
+            v-if="!isLoading" class="accounts card custom-card custom-card-padding"
           >
-            <h2 class="custom-card-title">Comptes en attente de validation</h2>
-            <table class="table is-striped is-fullwidth">
-              <thead>
+         <div v-if="pendingUserAccounts.length === 0">Aucun compte en attente de validation</div>
+         
+        <div v-else>
+         <h2 class="custom-card-title">Comptes en attente de validation</h2>
+          <table   class="table is-striped is-fullwidth">
+           <thead>
                 <tr>
                   <th>Utilisateur</th>
                   <th class="has-text-right">Valider</th>
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="account in pendingUserAccounts">
+                <tr v-for="account in pendingUserAccounts" :key="account.id">
                   <td>
                     {{account.name}} {{ account.markBackend ? `(via ${account.backendId})` : ""}}
                   </td>
@@ -30,7 +36,8 @@
                 </tr>
               </tbody>
             </table>
-          </div>
+        </div>
+      </div>
         </div>
       </div>
     </div>
@@ -39,22 +46,28 @@
 
 <script lang="ts">
   import { Options, Vue } from 'vue-class-component';
+    import Loading from 'vue-loading-overlay';
 
-  @Options({
-    name:"PendingAccounts",
-    mounted() {
-      this.$store.dispatch('fetchPendingUserAccounts')
-    },
-    computed: {
-      pendingUserAccounts(): Array<any> {
-        return this.$store.state.lokapi.pendingUserAccounts
-      }
-    },
-    methods: {
-      async validateUserAccount(account: any): Promise<void> {
-        try {
-          await account.validateCreation()
-        } catch (err) {
+@Options({
+  name:"PendingAccounts",
+  components :{ Loading: Loading },
+  data() {
+    return {
+      isLoading: false,
+    }
+  },
+  mounted() {
+    this.updatePendingAccount()
+  },
+  computed: {
+    pendingUserAccounts(): Array<any> {
+      return this.$store.state.lokapi.pendingUserAccounts
+    }
+  },
+  methods: {
+    validateUserAccount(account: any):void {
+      account.validateCreation()
+        .catch((err: any) => {
           this.$Swal.fire({
             position: 'top',
             icon: 'error',
@@ -62,19 +75,23 @@
             showConfirmButton: false,
             timer: 3000
           })
-          throw err
-        }
-
-        this.$store.dispatch('fetchPendingUserAccounts')
-        this.$Swal.fire({
-          position: 'top',
-          icon: 'success',
-          title: 'Le compte de l\'utilisateur ' + account.name + ' a été validé',
-          showConfirmButton: false,
-          timer: 3000
+        }).then((result: any) => {
+          this.updatePendingAccount()
+          this.$Swal.fire({
+            position: 'top',
+            icon: 'success',
+            title: 'Le compte de l\'utilisateur ' + account.name + ' a été validé',
+            showConfirmButton: false,
+            timer: 3000
+          })
         })
-      }
     },
-  })
-  export default class Admin extends Vue {}
+    async updatePendingAccount(){
+      this.isLoading = true
+      await this.$store.dispatch('fetchPendingUserAccounts')
+      this.isLoading = false
+    }
+  },
+})
+export default class Admin extends Vue {}
 </script>
