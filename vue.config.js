6d4f723aa0fc57373461a6ae5277a4c30efc2a8d
gwen <gwen@otavu.com>
2022-03-15 15:54:01 +0100

    new: add a system to retrieve the sass variables from the config.js file
    ... or from a specific config.theme.scss file in the public directory.
    
    The order of the tasks of the workflow file had to be changed because
    otherwise the build would not pick up the public/config.js file. All in
    all, everything works the same as before this commit.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const path =  require('path')
+const fs =  require('fs')
+
 // vue.config.js
 module.exports = {
   devServer: {
@@ -10,6 +13,25 @@ module.exports = {
     // can be string or regex
     /@lokavaluto\/.*/,
     /@0k.io\/.*/
-  ]
+  ],
+  configureWebpack: {
+    module: {
+      rules: [{
+        test: /\.scss$/,
+        use: [
+          { loader: "@epegzz/sass-vars-loader", options: {
+            syntax: 'scss',
+            // Variables from the config file
+            vars: require(path.resolve(__dirname, 'public/config.json')).theme,
+            // Variables from an optional separate scss file
+            files: (function() {
+              let sassConfigFilePath = path.resolve(__dirname, 'public/config.theme.scss')
+              return fs.existsSync(sassConfigFilePath) ? [ sassConfigFilePath ] : []
+            })()
+          }
+        }]
+      }]
+    }
+  }
 };
 
