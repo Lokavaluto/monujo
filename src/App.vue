29bc27e5b6f080cb3477d0858bf9abeb827c494c
Gwen Coffy <gwen@otavu.com>
2022-03-28 18:57:02 +0200

    chg: make the autologin not report error on auth failure

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 </template>
 
 <script lang="ts">
+  import { RestExc } from '@lokavaluto/lokapi-browser'
   import { Options, Vue } from 'vue-class-component';
   import Nav from "@/components/Nav.vue";
 
@@ -15,7 +16,17 @@
           console.log("Trying to Autolog")
           await this.$store.dispatch("initAutoLogin")
         } catch(e) {
-          console.error("Error while trying to autolog", e)
+          if (e instanceof RestExc.TokenRequired) {
+            // TokenRequired is cast by lokapi as one of 2 mecanism to
+            // react upon failure of auto-login. As we are already using
+            // the ``requestLogin`` callback, and that this callback issues
+            // a ``router.push("/")`` that won't do anything when already
+            // on this URL, this code will be run and this exception will
+            // be thrown and catched here. Notice that if the login page
+            // was on the URL "/login", that would not be necessary.
+            return
+          }
+          console.log("Error while trying to autolog", e)
         }
       }
     },
