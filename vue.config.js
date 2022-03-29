4b51da88ba7e5d072f5401af32b24d82fa910922
Valentin Lab <valentin.lab@kalysto.org>
2022-03-29 18:17:24 +0200

    new: pkg: update all version to latest available
    Remove all vulnerability reports.
    Commit ``package-lock.json`` as this is a root app.
    
    Signed-off-by: Valentin Lab <valentin.lab@kalysto.org>

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,15 @@
 const path =  require('path')
 const fs =  require('fs')
+const webpack = require("webpack")
 
 // vue.config.js
 module.exports = {
   devServer: {
-    watchOptions: {
-      ignored: ['**/.#*', 'node_modules/**']
-    }
+    static: {
+      watch: {
+        ignored: ["**/.#*", "node_modules/**"],
+      },
+    },
   },
   lintOnSave: false,
   transpileDependencies: [
@@ -15,6 +18,24 @@ module.exports = {
     /@0k.io\/.*/
   ],
   configureWebpack: {
+    resolve: {
+      fallback: {
+        crypto: require.resolve("crypto-browserify"),
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        stream: require.resolve("stream-browserify"),
+        assert: require.resolve("assert/"),
+        url: require.resolve("url/"),
+        buffer: require.resolve("buffer/"),
+        process: require.resolve("process/"),
+      },
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ["buffer", "Buffer"],
+        process: ["process"],
+      }),
+    ],
     module: {
       rules: [{
         test: /\.scss$/,
