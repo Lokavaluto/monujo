dc5a50224e8ac0fd49e813fa99134178b3c153c9
Gwen Coffy <gwen@otavu.com>
2022-03-30 12:13:23 +0200

    new: add a loading indicator to the pending credits list

--- src/views/admin/PendingCredits.vue
+++ src/views/admin/PendingCredits.vue
@@ -3,52 +3,55 @@
     <div class="container mt-5">
       <div class="columns is-tablet">
         <div class="column">
-          <div
-            class="accounts card custom-card custom-card-padding"
-          >
-            <div class="notification is-danger is-light" v-if="hasLoadingError">
-              <p class="mb-4">Une erreur inattendue est survenue pendant le chargement de la liste de demandes de crédits. Veuillez nous excuser pour le désagrément.</p>
-              <p>Vous pouvez essayer de recharger la page, ou contacter votre administrateur si l'erreur persiste.</p>
-            </div>
-            <p
-              class="notification is-default"
-              v-else-if="pendingCreditRequests.length === 0"
-            >Aucune demande de crédit en attente de validation</p>
-            <div v-else>
-              <h2 class="custom-card-title">Opérations de crédit en attente de validation</h2>
-              <table class="table is-striped is-fullwidth">
-                <thead>
-                  <tr>
-                    <th>Utilisateur</th>
-                    <th>Montant</th>
-                    <th class="has-text-right">Valider</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="request in pendingCreditRequests">
-                    <td>
-                      {{request.relatedUser}} {{ request.markBackend ? `(on ${request.backendId})` : ""}}
-                    </td>
-                    <td>
-                      {{
-                        parseFloat(request.amount).toLocaleString(
-                          "fr", {
-                            minimumFractionDigits: 2,
-                            maximumFractionDigits: 2
-                          })
-                      }} {{request.currency}}
-                    </td>
-                    <td class="has-text-right">
-                      <a
-                        class="button is-primary custom-button custom-inverted is-small is-pulled-right"
-                        v-on:click="validateCreditRequest(request)"
-                      >
-                        valider
-                      </a>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+          <div class="accounts card custom-card custom-card-padding">
+            <loading v-model:active="isLoading"
+                 :can-cancel="false"
+                 :is-full-page= "false"/>
+            <div v-if="!isLoading">
+              <div class="notification is-danger is-light" v-if="hasLoadingError">
+                <p class="mb-4">Une erreur inattendue est survenue pendant le chargement de la liste de demandes de crédits. Veuillez nous excuser pour le désagrément.</p>
+                <p>Vous pouvez essayer de recharger la page, ou contacter votre administrateur si l'erreur persiste.</p>
+              </div>
+              <p
+                class="notification is-default"
+                v-else-if="pendingCreditRequests.length === 0"
+              >Aucune demande de crédit en attente de validation</p>
+              <div v-else>
+                <h2 class="custom-card-title">Opérations de crédit en attente de validation</h2>
+                <table class="table is-striped is-fullwidth">
+                  <thead>
+                    <tr>
+                      <th>Utilisateur</th>
+                      <th>Montant</th>
+                      <th class="has-text-right">Valider</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="request in pendingCreditRequests">
+                      <td>
+                        {{request.relatedUser}} {{ request.markBackend ? `(on ${request.backendId})` : ""}}
+                      </td>
+                      <td>
+                        {{
+                          parseFloat(request.amount).toLocaleString(
+                            "fr", {
+                              minimumFractionDigits: 2,
+                              maximumFractionDigits: 2
+                            })
+                        }} {{request.currency}}
+                      </td>
+                      <td class="has-text-right">
+                        <a
+                          class="button is-primary custom-button custom-inverted is-small is-pulled-right"
+                          v-on:click="validateCreditRequest(request)"
+                        >
+                          valider
+                        </a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
@@ -59,11 +62,14 @@
 
 <script lang="ts">
   import { Options, Vue } from 'vue-class-component';
+  import Loading from 'vue-loading-overlay';
 
   @Options({
     name:"PendingCredits",
+    components:{ Loading: Loading },
     data() {
       return {
+        isLoading: true,
         hasLoadingError: false,
       }
     },
@@ -99,6 +105,7 @@
         })
       },
       async updatePendingCreditRequests() {
+        this.isLoading = true
         try {
           await this.$store.dispatch('fetchPendingCreditRequests')
           this.hasLoadingError = false
@@ -106,6 +113,7 @@
           console.error('Failed to fetch pending credit requests', e)
           this.hasLoadingError = true
         }
+        this.isLoading = false
       }
     },
   })
