ecdaa4b8b56d2c6b754e4b6fe27ff8fe8372a809
Seddik Kadi <seddik.kadi@outlook.fr>
2022-04-25 11:50:44 +0200

    fix: prevent scroll underneath modals (fixes: #181)

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,8 +7,13 @@ import { createStore } from "vuex"
 export default createStore({
   state: {
     showCredit: false,
+    isModalOpen: false,
   },
-  mutations: {},
+  mutations: {
+    setModalState(state: any, modalState: boolean) {
+      state.isModalOpen = modalState
+    },
+  },
   actions: {},
   modules: {},
   getters: {},
