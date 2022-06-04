b71b3489a759d7dff970cc90432708b4568cb223
Valentin Lab <valentin.lab@kalysto.org>
2022-06-04 16:46:49 +0200

    fix: trigger global loading only after user provided credentials
    Signed-off-by: Valentin Lab <valentin.lab@kalysto.org>

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,10 +8,15 @@ export default createStore({
   state: {
     showCredit: false,
     isModalOpen: false,
+    requestLoadingAfterCreds: false,
   },
   mutations: {
     setModalState(state: any, modalState: boolean) {
       state.isModalOpen = modalState
+    },
+
+    setRequestLoadingAfterCreds(state: any, requestLoadingAfterCreds: boolean) {
+      state.requestLoadingAfterCreds = requestLoadingAfterCreds
     },
   },
   actions: {},
