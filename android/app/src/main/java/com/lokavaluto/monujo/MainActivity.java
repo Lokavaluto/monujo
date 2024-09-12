4ee322fa87c79518cdf1c6b4149324b4fa83acec
Valentin Lab <valentin.lab@kalysto.org>
2024-09-12 10:25:03 +0200

    fix: pkg: repair android 14 fail on app launch

--- android/app/src/main/java/com/lokavaluto/monujo/MainActivity.java
+++ android/app/src/main/java/com/lokavaluto/monujo/MainActivity.java
@@ -2,4 +2,21 @@ package com.lokavaluto.monujo;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import org.jetbrains.annotations.Nullable;
+import android.content.Context;
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public Intent registerReceiver(@Nullable BroadcastReceiver receiver, IntentFilter filter) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            return super.registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED);
+        } else {
+            return super.registerReceiver(receiver, filter);
+        }
+    }
+}
