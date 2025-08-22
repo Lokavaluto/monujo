ab60f4dc3c2a12c50de11b030c6f2f7047b8d38c
Valentin Lab <valentin.lab@kalysto.org>
2025-08-22 10:15:59 +0200

    fix: dev: add polyfill to make android compatible with ``.throwIfAborted``

--- src/polyfill.ts
+++ src/polyfill.ts
@@ -1,3 +1,4 @@
+// Promise.allSettled
 Promise.allSettled =
   Promise.allSettled ||
   ((promises: any) =>
@@ -14,3 +15,39 @@ Promise.allSettled =
           }))
       )
     ))
+
+// AbortSignal.throwIfAborted
+;(function () {
+  if (
+    typeof AbortSignal !== "undefined" &&
+    !AbortSignal.prototype.throwIfAborted
+  ) {
+    const makeAbortError = (cause?: any) => {
+      try {
+        const err = new DOMException("The operation was aborted.", "AbortError")
+        if (cause !== undefined) {
+          try {
+            Object.defineProperty(err, "cause", {
+              value: cause,
+              configurable: true,
+            })
+          } catch {}
+        }
+        return err
+      } catch {
+        const err = new Error("The operation was aborted.")
+        err.name = "AbortError"
+        if (cause !== undefined) err.cause = cause
+        return err
+      }
+    }
+
+    AbortSignal.prototype.throwIfAborted = function () {
+      if (!this.aborted) return
+      // Spec behavior: throw the *existing reason* if present; else AbortError DOMException
+      const r = this.reason
+      if (r === undefined) throw makeAbortError()
+      throw r
+    }
+  }
+})()
